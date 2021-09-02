6491b489b85d17da162555b461f358678ceaaa4f
Shrikant Yadav <shrikant9907@gmail.com>
2021-09-03 00:10:37 +0530

    slider in homes get index page

--- sites/realestate/homesget/js/custom.js
+++ sites/realestate/homesget/js/custom.js
@@ -1,4 +1,26 @@
-$(document).ready(function(e){
-    e.preventDefault();
-    alert(ajax_object.ajax_url);
+jQuery(document).ready(function(e){    
+     // Home Slider
+     jQuery('.home-slider').slick({
+        centerMode: false, 
+        centerPadding: '0',
+        slidesToShow: 1,
+        responsive: [
+            {
+            breakpoint: 768, 
+            settings: {
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow: 1
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow: 1
+            }
+            }
+        ]
+    }); 
 });
