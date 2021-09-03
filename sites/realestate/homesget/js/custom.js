5c30fd8f140cb5679a05cb258c8c214005443d8d
Shrikant Yadav <shrikant9907@gmail.com>
2021-09-03 11:27:15 +0530

    fixes

--- sites/realestate/homesget/js/custom.js
+++ sites/realestate/homesget/js/custom.js
@@ -24,5 +24,19 @@ jQuery(document).ready(function(e){
         ]
     }); 
   
-   
+    // init Isotope
+    var $grid = $('.grid').isotope({
+        itemSelector: '.filter-items'
+    });
+    $('.filters').on( 'click', 'button', function( event ) {
+        var $target = $( event.currentTarget );
+        $target.siblings().removeClass('is-checked');
+        $target.toggleClass('is-checked');
+        var isChecked = $target.hasClass('is-checked');
+        var filter = $target.attr('data-filter');
+        if (!isChecked)  {
+        filter = '*';
+        }
+        $grid.isotope({ filter: filter });
+    });
 });
